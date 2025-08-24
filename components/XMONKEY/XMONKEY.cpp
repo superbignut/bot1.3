e3da3f921a792528a91378212f8c7751c9695fcc
bignut <1214002133@qq.com>
2025-08-24 20:53:28 +0800

    robot-exec and leg-exec added.

--- components/XMONKEY/XMONKEY.cpp
+++ components/XMONKEY/XMONKEY.cpp
@@ -13,9 +13,10 @@
 #include "esp_timer.h"
 #include "PCA9685.h"
 
-void INNER_MOTOR::set_motor_angle(float angle)
+/* void INNER_MOTOR::set_motor_angle(float angle)
 {
-    MY_PCA9685_SET_ANGLE(_leg_index, angle - _offset);
+    _angle = angle;
+    
 }
 INNER_MOTOR::INNER_MOTOR()
 {
@@ -28,9 +29,15 @@ INNER_MOTOR::INNER_MOTOR(int leg_index, int offset)
     _offset = offset;
 }
 
+void INNER_MOTOR::motor_exec()
+{
+    MY_PCA9685_SET_ANGLE(_leg_index, _angle - _offset);
+}
+
 void OUTER_MOTOR::set_motor_angle(float angle)
 {
-    MY_PCA9685_SET_ANGLE(_leg_index, angle - _offset);
+    _angle = angle;
+    // MY_PCA9685_SET_ANGLE(_leg_index, angle - _offset);
 }
 
 OUTER_MOTOR::OUTER_MOTOR()
@@ -44,6 +51,11 @@ OUTER_MOTOR::OUTER_MOTOR(int leg_index, int offset)
     _offset = offset;
 }
 
+void OUTER_MOTOR::motor_exec()
+{
+    MY_PCA9685_SET_ANGLE(_leg_index, _angle - _offset);
+} */
+
 LEG::LEG()
 {
     
@@ -53,13 +65,38 @@ LEG::LEG(int in_index,int in_offset,int ot_index,int ot_offset)
 
 }
 
-void LEG::set_leg(int in_index,int in_offset,int ot_index,int ot_offset)
+/// @brief 根据落脚点的位置， 计算 内外舵机的角度，
+///        并将解算的角度， 直接替换参数 locale_x <-> inner_angle   locale_y <-> oouter_angle
+/// @param locale_x 
+/// @param locale_y 
+/// @param locale_z 
+void LEG::trans_from_position_to_angle(int leg_index, float *locale_x, float *locale_y, float locale_z)// <---- todo  解算
 {
-    in_motor.set_leg_index(in_index);
-    in_motor.set_leg_offset(in_offset);
 
-    ot_motor.set_leg_index(ot_index);
-    ot_motor.set_leg_offset(ot_offset);
+}
+
+/// @brief 将 robot 下发的 位置坐标转为 角度坐标， 进而下发给舵机
+/// @param leg_index 
+/// @param locale_x 
+/// @param locale_y 
+/// @param locale_z 
+void LEG::leg_exec(int leg_index, float locale_x, float locale_y, float locale_z)
+{
+    trans_from_position_to_angle(leg_index, &locale_x, &locale_y, locale_z); // 得到结算后的角度
+    _in_motor_angle = locale_x;
+    _ot_motor_angle = locale_y;
+    
+    MY_PCA9685_SET_ANGLE(_in_motor_index, _in_motor_angle - _in_motor_offset);
+    MY_PCA9685_SET_ANGLE(_ot_motor_index, _ot_motor_angle - _ot_motor_offset); // 这里 分两次下发, 太难受了 <---- todo 改成一次下发
+}
+
+void LEG::set_leg(int in_index, int in_offset, int ot_index, int ot_offset)
+{
+    _in_motor_index = in_index;
+    _in_motor_offset = in_offset;
+
+    _ot_motor_index = ot_index;
+    _ot_motor_offset = ot_offset;
 }
 
 
@@ -77,10 +114,12 @@ MONKEY::MONKEY()
     _leg[3].set_leg(7, 0, 3, 0);  // leg 初始化 编号和offset
 
 
-    set_leg_rela(LEG_0, 0, 0);
-    set_leg_rela(LEG_1, 0, 0);  // <--------------- Todo 
-    set_leg_rela(LEG_2, 0, 0);
-    set_leg_rela(LEG_3, 0, 0);
+    set_leg_rela(LEG_0, -HALF_ROBOT_WIDTH, -HALF_ROBOT_LENGTH);
+    set_leg_rela(LEG_1,  HALF_ROBOT_WIDTH, -HALF_ROBOT_LENGTH);  // <--------------- Todo 
+    set_leg_rela(LEG_2,  HALF_ROBOT_WIDTH,  HALF_ROBOT_LENGTH);
+    set_leg_rela(LEG_3, -HALF_ROBOT_WIDTH,  HALF_ROBOT_LENGTH);
+
+    reset();
 
     printf("init...\n");    
     
@@ -97,16 +136,45 @@ void MONKEY::set_leg_rela(int leg_index, float rela_x, float rela_y)
 
 
 
+
+/// @brief 
+/*  
+
+                leg1                    robot (HALF_ROBOT_WIDTH,  0,  0)
+                  ____________________________.
+                 /|
+                / |
+               /  |
+        40    /   |   33
+             /    |         arctan(20 / 33) = 30度
+            /     |
+           /      |
+          /       |
+         ---------|
+        |    20
+   20   |
+        |
+        |
+        *  Foot
+*/
 void MONKEY::reset()
 {
-
+    //set_leg_global_position(0, )
 }
 
 
 /// @brief monkey 所有 leg 初始化
 void MONKEY::reset()
 {
+    set_leg_position(LEG_0, -HALF_ROBOT_WIDTH, -HALF_ROBOT_LENGTH, -53); // -53 = 20 + 40 * cos\theta
 
+    set_leg_position(LEG_1,  HALF_ROBOT_WIDTH, -HALF_ROBOT_LENGTH, -53); // -53 = 20 + 40 * cos\theta
+
+    set_leg_position(LEG_2,  HALF_ROBOT_WIDTH,  HALF_ROBOT_LENGTH, -53); // -53 = 20 + 40 * cos\theta
+
+    set_leg_position(LEG_3, -HALF_ROBOT_WIDTH,  HALF_ROBOT_LENGTH, -53); // -53 = 20 + 40 * cos\theta
+
+    robot_exec();
 }
 
 void MONKEY::walk(float steps, int T = 500)
@@ -114,4 +182,78 @@ void MONKEY::walk(float steps, int T = 500)
     printf("walk...\n");
 
 
+}
+
+
+/*
+
+
+                （x，y, z）
+                /
+               /
+              /
+             /
+           |/
+           ```
+          leg-1-upper-point (x - HALF-ROBOT-WIDTH, y + HALF-ROBOT-LENGTH, z)
+
+
+*/
+
+/// @brief Change the foot-global-relative-robot position to leg-upper-point-locale position.
+///        把机器人 腿末端的， 相对机器人中心参考系的坐标， 转换为， 以那条腿和机器人连接点为局部坐标原点的 相对坐标
+/// @param leg_index 
+/// @param leg_global_x 
+/// @param leg_global_y 
+/// @param leg_global_z 
+void MONKEY::trans_from_robot_to_leg(int leg_index, float *leg_global_x, float *leg_global_y, float *leg_global_z)
+{
+    assert(leg_index >=0 && leg_index < LEG_NUM);
+    switch (leg_index)
+    {
+    case LEG_0:
+        *leg_global_x = *leg_global_x + HALF_ROBOT_WIDTH;
+        *leg_global_y = *leg_global_y + HALF_ROBOT_LENGTH;
+        *leg_global_z = *leg_global_z;
+        break;
+    case LEG_1:
+        *leg_global_x = *leg_global_x - HALF_ROBOT_WIDTH;
+        *leg_global_y = *leg_global_y + HALF_ROBOT_LENGTH;
+        *leg_global_z = *leg_global_z;
+        break;
+
+    case LEG_2:
+        *leg_global_x = *leg_global_x - HALF_ROBOT_WIDTH;
+        *leg_global_y = *leg_global_y - HALF_ROBOT_LENGTH;
+        *leg_global_z = *leg_global_z;
+        break;
+
+    case LEG_3:
+        *leg_global_x = *leg_global_x + HALF_ROBOT_WIDTH;
+        *leg_global_y = *leg_global_y - HALF_ROBOT_LENGTH;
+        *leg_global_z = *leg_global_z;
+        break;
+    
+    default:
+        break;
+    }
+}
+
+void MONKEY::set_leg_position(int leg_index, float leg_global_x, float leg_global_y, float leg_global_z)
+{   
+    trans_from_robot_to_leg(leg_index, &leg_global_x, &leg_global_y, &leg_global_z);
+    
+    _leg_locale_position[leg_index][0] = leg_global_x;
+    _leg_locale_position[leg_index][1] = leg_global_y;
+    _leg_locale_position[leg_index][2] = leg_global_z;
+}
+
+/// @brief 机器人执行指令
+void MONKEY::robot_exec()
+{   
+    // 四条腿执行陆续指令
+    for(int i=0; i<LEG_NUM; ++i)
+    {
+        _leg[i].leg_exec(i, _leg_locale_position[i][0], _leg_locale_position[i][1], _leg_locale_position[i][2]);
+    }
 }
--- components/XMONKEY/include/XMONKEY.h
+++ components/XMONKEY/include/XMONKEY.h
@@ -65,9 +65,16 @@ outer motor:  0-1-2-3 is rotate around x's relative axis
 #define LEG_2 2
 #define LEG_3 3
 
+#define ROBOT_LENGTH (80.6)
+#define ROBOT_WIDTH (67.5)
+#define ROBOT_HIGH (30)
 
-class INNER_MOTOR
-{
+#define HALF_ROBOT_LENGTH (80.6 / 2)
+#define HALF_ROBOT_WIDTH (67.5 / 2)
+#define HALF_ROBOT_HIGH (30 / 2)
+
+
+/*class INNER_MOTOR {
 
 public:
     INNER_MOTOR();
@@ -77,14 +84,14 @@ public:
 
     void set_leg_offset(int offset);
 
-private:
     void set_motor_angle(float angle);
 
-
+    void motor_exec();
 
 private:
     int _leg_index = 5;
     int _offset = 0;
+    float _angle;
 };
 
 class OUTER_MOTOR
@@ -97,14 +104,17 @@ public:
 
     void set_leg_offset(int offset);
 
-private:
     void set_motor_angle(float angle);
+
+    void motor_exec();
 
 private:
     int _leg_index = 5;
     int _offset = 0;
-};
 
+    float _angle;
+};
+ */
 class LEG
 {
 public:
@@ -112,12 +122,14 @@ public:
 
     LEG(int in_index,int in_offset,int ot_index,int ot_offset); // 初始化电机编号， 与位置补偿
 
-    void set_locale_position(float locale_x, float locale_y, float locale_z);    // 设置位置，当前参考系
+    void leg_exec(int leg_index, float locale_x, float locale_y, float locale_z);    // 设置位置，当前参考系
 
     void set_leg(int in_index,int in_offset,int ot_index,int ot_offset);
 
+
+
 private:
-    float* from_position_to_angle(float locale_x, float locale_y, float locale_z);// 将locale地址 转为角度坐标, 解算
+    void trans_from_position_to_angle(int leg_index, float *locale_x, float *locale_y, float locale_z);// 将locale地址 转为角度坐标, 解算
 
     void set_angle_position(float in_angle, float ot_angle);  // 设置角度， 下发 motor
 
@@ -125,8 +137,13 @@ private:
     
     float current_motor_pos[MOTOR_NUM / LEG_NUM]; // 当下电机的位置
 
-    INNER_MOTOR in_motor;   // 内侧电机
-    OUTER_MOTOR ot_motor;   // 外侧电机
+    int _in_motor_index = 5;
+    int _in_motor_offset = 0;
+    float _in_motor_angle;
+
+    int _ot_motor_index = 5;
+    int _ot_motor_offset = 0;
+    float _ot_motor_angle;
 };
 
 class MONKEY
@@ -138,20 +155,24 @@ public:
 
     void walk(float steps, int period); // 进入步态大小循环， 计算robot 坐标下 每个腿末态位置， 不进行接算
 
-    void from_robot_to_leg(); //  将robot 坐标转为 腿坐标
-
-    void set_leg_global_position(int leg_index, float leg_global_x, float leg_global_y, float leg_global_z);    // 将全局坐标转换为相对， 并下发给leg
-
 private:
 
-    void set_leg_rela(int leg_index, float rela_x, float rela_y);
+    void trans_from_robot_to_leg(int leg_index, float *leg_global_x, float *leg_global_y, float *leg_global_z); //  将robot 坐标转为 腿坐标
+
+    void set_leg_position(int leg_index, float leg_x, float leg_y, float leg_z);    // 将全局坐标转换为相对， 并下发给leg
+
+    void robot_exec();    // 运动下发
+
+    void set_leg_rela(int leg_index, float rela_x, float rela_y);   // 设置 leg 相对 body 的坐标
 
     LEG _leg[LEG_NUM];
 
     float _leg_rela_x[LEG_NUM];  // 每个腿 相对 robot 中心的位置坐标
     float _leg_rela_y[LEG_NUM];
     
-    float _global_position[3];   // robot's xyz 
+    float _robot_global_position[3];   // robot's xyz 
+
+    float _leg_locale_position[LEG_NUM][3];
 
     // float global_leg_position[LEG_NUM][3];  // 记录 腿 终点（与地面接触点） 的坐标
 };
